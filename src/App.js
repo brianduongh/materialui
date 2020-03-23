14d2d9d809265a52cb2e0b8e6ca97cefa5d2f583
Brian Duong <brianduongh@gmail.com>
2020-03-23 17:12:25 -0500

    Add material ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const styles = {
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+};
+
+function App(props) {
+  const { classes } = props;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +35,14 @@ function App() {
         >
           Learn React
         </a>
+        <Button className={classes.root}>Higher-order component</Button>
       </header>
     </div>
   );
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
